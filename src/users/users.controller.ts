ebb13cd49572e9172dde0a7f59f9019c4c8f644a
salman-tyagi <salmantyagi15@gmail.com>
2025-10-30 11:07:16 +0530

    create user using typeorm and mongodb

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,12 +3,12 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 
-@Controller('users')
+@Controller('auth')
 export class UsersController {
   constructor(private userService: UsersService) {}
 
-  @Post()
+  @Post('signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    this.userService.create(body.email, body.password);
   }
 }
