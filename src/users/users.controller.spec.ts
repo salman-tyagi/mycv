41aba502f9557b5da24e6a5b238076a5c3db93d5
salman-tyagi <salmantyagi15@gmail.com>
2025-11-05 16:34:21 +0530

    unit testing a controller started

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,10 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+
 describe('UsersController', () => {
   let controller: UsersController;
+  let usersService: UsersService;
+  let authService: AuthService;
 
   beforeEach(async () => {
+    authService = {
+      signup: () => {},
+      login: () => {},
+    };
+
+    usersService = {
+      findOne: () => {},
+      find: () => {},
+      update: () => {},
+      remove: () => {},
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
     }).compile();
