7744268ecb0257c9e4c3cb96f648c4a6b0d9a599
salman-tyagi <salmantyagi15@gmail.com>
2025-11-06 18:44:04 +0530

    dynamically change runtime environment per script

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const PORT = process.env.PORT ?? 3000;
+
+  await app.listen(PORT);
+  console.log(`Listening on port ${PORT} in ${process.env.NODE_ENV}`);
 }
 bootstrap();
