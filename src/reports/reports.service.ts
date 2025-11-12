f718753a832cc9971e284e9400fad8f9fc6a3e86
salman-tyagi <salmantyagi15@gmail.com>
2025-11-12 12:59:54 +0530

    builder query not working in mongodb, inject datasource for mongorepository

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,15 +1,96 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { ObjectId, Repository } from 'typeorm';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { ObjectId, DataSource, MongoRepository } from 'typeorm';
 
 import { Report } from './report.entity';
 import { User } from '../users/user.entity';
 
 import { CreateReportDto } from './dtos/create-report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Injectable()
 export class ReportsService {
-  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+  // constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  private repo: MongoRepository<Report>;
+  constructor(@InjectDataSource() private dataSource: DataSource) {
+    this.repo = this.dataSource.getMongoRepository(Report);
+  }
+
+  async createEstimate({ make, model, lng, lat, year, mileage }: GetEstimateDto) {
+    const latLngDelta = 5;
+    const yearDelta = 3;
+    const mileageDelta = 5000;
+
+    const [stats] = await this.repo
+      .aggregate([
+        {
+          $match: {
+            approved: true,
+            make: make,
+            model: model,
+            lat: {
+              $gte: lat - latLngDelta,
+              $lte: lat + latLngDelta,
+            },
+            lng: {
+              $gte: lng - latLngDelta,
+              $lte: lng + latLngDelta,
+            },
+            year: {
+              $gte: year - yearDelta,
+              $lte: year + yearDelta,
+            },
+            mileage: {
+              $gte: mileage - mileageDelta,
+              $lte: mileage + mileageDelta,
+            },
+          },
+        },
+        {
+          $sort: {
+            mileage: -1,
+          },
+        },
+        {
+          $limit: 3,
+        },
+        {
+          $group: {
+            _id: null,
+            averagePrice: { $avg: '$price' },
+          },
+        },
+      ])
+      .toArray();
+
+    return { price: stats ? stats.averagePrice : 0 };
+
+    // return this.repo.find({
+    //   where: {
+    //     make: make,
+    //     model: model,
+    //     lat: {
+    //       $gte: lat - latLngDelta,
+    //       $lte: lat + latLngDelta,
+    //     },
+    //     lng: {
+    //       $gte: lng - latLngDelta,
+    //       $lte: lng + latLngDelta,
+    //     },
+    //     year: {
+    //       $gte: year - yearDelta,
+    //       $lte: year + yearDelta,
+    //     },
+    //     mileage: {
+    //       $gte: mileage - mileageDelta,
+    //       $lte: mileage + mileageDelta,
+    //     },
+    //   },
+    //   order: { mileage: 'DESC' },
+    //   take: 3,
+    // });
+  }
 
   create(reportDto: CreateReportDto, user: User) {
     const report = this.repo.create({ ...reportDto, user });
